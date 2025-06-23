8fa3aee9633563f465693100c969ca6cfcef1101
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-06-23 17:34:57 +0300

    checkout including Contact, delivery, shipping and payment forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CheckoutComponent } from './components/checkout-form/checkout-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: "checkout", component:CheckoutComponent , title: "Payment Form"  },
+      { path: "**", component:NotFoundComponent , title: "Error 404"  },
+
+
+];
