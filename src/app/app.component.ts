76b5484f6c2fe454adccfcd88be1f192233c98eb
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2025-06-24 18:40:43 +0300

    implementing product card component ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FilterComponent } from './components/filter/filter.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilterComponent],
+  imports: [RouterOutlet, FilterComponent, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
