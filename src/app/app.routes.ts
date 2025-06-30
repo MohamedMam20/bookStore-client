22305d3a31d90b4935ba293721df570cb98adfea
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2025-06-30 18:04:13 +0300

    fetchig products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CheckoutComponent } from './components/checkout-form/checkout-form.component';
-<<<<<<< productCard
-import { BooksPageComponent } from './pages/books-page/books-page.component';
-=======
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegistrationComponent } from './pages/auth/registration/registration.component';
 import { OtpVerificationComponent } from './pages/auth/otp-verfication/otp-verfication.component';
@@ -13,10 +10,11 @@ import { PasswordResetRequestComponent } from './pages/auth/password-reset-reque
 import { PasswordResetConfirmComponent } from './pages/auth/password-reset-confirm/password-reset-confirm.component';
 import { OtpCompleteComponent } from './pages/auth/otp-complete/otp-complete.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
->>>>>>> main
+import { BooksPageComponent } from './pages/books-page/books-page.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: UploadComponent },
   { path: 'wishlist', component: WishlistComponent },
   { path: 'shop', component: BooksPageComponent },
   { path: 'checkout', component: CheckoutComponent, title: 'Payment Form' },
@@ -26,7 +24,6 @@ export const routes: Routes = [
   { path: 'otp-complete', component: OtpCompleteComponent },
   { path: 'password-reset', component: PasswordResetRequestComponent },
   { path: 'password-reset-confirm', component: PasswordResetConfirmComponent },
-
   { path: 'product-details', component: ProductDetailsComponent },
   { path: '**', component: NotFoundComponent },
 ];
