ab47a530ecd3366f922355b75bc4d9ad6d1bcd24
narriman <narimanamer805@gmail.com>
2025-07-01 12:43:34 +0300

    Home page enhancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 // import { NavbarComponent } from './components/navbar-components/navbar/navbar.component';
 
 
