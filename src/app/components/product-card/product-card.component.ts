76645983ef4e2a6336b8d3f8b9cd83367ccc9b42
Sara Ashraf <madboulysara900@gmail.com>
2025-07-02 22:46:19 +0300

    productdetails finished

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -3,15 +3,33 @@ import { CommonModule } from '@angular/common';
 import { Book } from '../../models/book.model';
 import { CartService } from '../../services/cart/cart.service';
 import { ToastrService } from 'ngx-toastr';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true, // ⬅️ add this
-  imports: [CommonModule], // ⬅️ needed for currency pipe, ngIf, ngFor
+  imports: [CommonModule,RouterModule], // ⬅️ needed for currency pipe, ngIf, ngFor
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent {
   @Input() product!: Book;
+  onBoltClick(event: MouseEvent) {
+  event.stopPropagation();
+  event.preventDefault();
+  // TODO: Add functionality for bolt
+}
+
+onCartClick(event: MouseEvent) {
+  event.stopPropagation();
+  event.preventDefault();
+  // TODO: Add to cart logic here
+}
+
+onHeartClick(event: MouseEvent) {
+  event.stopPropagation();
+  event.preventDefault();
+  // TODO: Add to wishlist logic here
+}
 
 }
