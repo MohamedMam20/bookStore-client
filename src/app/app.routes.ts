9e4148192227d18f8ea3b14f36ee43c4658af690
Mohamed Mamdouh <MohamedMam20@gmail.com>
2025-07-21 05:37:13 +0300

    final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,36 @@
 import { Routes } from '@angular/router';
-import { WishlistComponent } from './pages/wishlist/wishlist.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { RegistrationComponent } from './pages/auth/registration/registration.component';
-import { OtpVerificationComponent } from './pages/auth/otp-verfication/otp-verfication.component';
-import { PasswordResetRequestComponent } from './pages/auth/password-reset-request/password-reset-request.component';
-import { PasswordResetConfirmComponent } from './pages/auth/password-reset-confirm/password-reset-confirm.component';
-import { OtpCompleteComponent } from './pages/auth/otp-complete/otp-complete.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
-import { BooksPageComponent } from './pages/books-page/books-page.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { AdminComponent } from './pages/admin/admin.component';
-import { BookListComponent } from './pages/admin/components/book-list/book-list.component';
-import { BookFormComponent } from './pages/admin/components/book-form/book-form.component';
-import { DashboardOverviewComponent } from './pages/admin/components/dashboard-overview/dashboard-overview.component';
-import { UserListComponent } from './pages/admin/components/user-list/user-list.component';
-import { UserFormComponent } from './pages/admin/components/user-form/user-form.component';
-import { MyAccountComponent } from './pages/my-account/my-account.component';
-import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
-import { OrderConfirmationComponent } from './components/order-confirmation/order-confirmation.component'; // Add this import
-import { SearchBooksPageComponent } from './pages/search-books-page/search-books-page.component';
-import { PaypalComponent } from './pages/paypal/paypal.component';
-import { OrderHistoryComponent } from './pages/order-history/order-history.component';
-import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AdminGuard } from './guards/admin.guard';
-// import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FAQPageComponent } from './pages/faq-page/faq-page.component';
-import { AdminListComponent } from './pages/admin/components/admin-list/admin-list.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
+
 export const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'contact-us', component: ContactUsComponent, title: 'contactus' },
-  { path: 'faq', component: FAQPageComponent, title: 'FAQ' },
-  { path: 'paypal', component: PaypalComponent, title: 'paypal' },
-
-  // // export const routes: Routes = [
-  //   { path: '', component: HomePageComponent },
-  //   { path: 'paypal', component: PaypalComponent, title: 'paypal' },
-
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home-page/home-page.component').then(
+        (m) => m.HomePageComponent
+      ),
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () =>
+      import('./pages/contact-us/contact-us.component').then(
+        (m) => m.ContactUsComponent
+      ),
+    title: 'contactus',
+  },
+  {
+    path: 'faq',
+    loadComponent: () =>
+      import('./pages/faq-page/faq-page.component').then(
+        (m) => m.FAQPageComponent
+      ),
+    title: 'FAQ',
+  },
+  {
+    path: 'paypal',
+    loadComponent: () =>
+      import('./pages/paypal/paypal.component').then((m) => m.PaypalComponent),
+    title: 'paypal',
+  },
   {
     path: 'thank-you',
     loadComponent: () =>
@@ -45,7 +38,14 @@ export const routes: Routes = [
         (m) => m.ThankYouComponent
       ),
   },
-  { path: 'orders', component: OrderHistoryComponent, title: 'My Orders' },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('./pages/order-history/order-history.component').then(
+        (m) => m.OrderHistoryComponent
+      ),
+    title: 'My Orders',
+  },
   {
     path: 'user-profile',
     loadComponent: () =>
@@ -54,42 +54,171 @@ export const routes: Routes = [
       ),
     title: 'My Profile',
   },
-
-  { path: 'contact-us', component: ContactUsComponent },
-  { path: 'wishlist', component: WishlistComponent },
-  { path: 'shop', component: BooksPageComponent },
-  { path: 'checkout', component: CheckoutFormComponent, title: 'Payment Form' },
+  {
+    path: 'wishlist',
+    loadComponent: () =>
+      import('./pages/wishlist/wishlist.component').then(
+        (m) => m.WishlistComponent
+      ),
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./pages/books-page/books-page.component').then(
+        (m) => m.BooksPageComponent
+      ),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./components/checkout-form/checkout-form.component').then(
+        (m) => m.CheckoutFormComponent
+      ),
+    title: 'Payment Form',
+  },
   {
     path: 'order-confirmation',
-    component: OrderConfirmationComponent,
+    loadComponent: () =>
+      import(
+        './components/order-confirmation/order-confirmation.component'
+      ).then((m) => m.OrderConfirmationComponent),
     title: 'Order Confirmation',
-  }, // Added route
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: 'otp-verification', component: OtpVerificationComponent },
-  { path: 'otp-complete', component: OtpCompleteComponent },
-  { path: 'password-reset', component: PasswordResetRequestComponent },
-  { path: 'password-reset-confirm', component: PasswordResetConfirmComponent },
-  { path: 'product-details', component: ProductDetailsComponent },
-  { path: 'my-account', component: MyAccountComponent },
-  { path: 'search', component: SearchBooksPageComponent },
-  { path: 'aboutus', component: AboutUsComponent },
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/auth/registration/registration.component').then(
+        (m) => m.RegistrationComponent
+      ),
+  },
+  {
+    path: 'otp-verification',
+    loadComponent: () =>
+      import('./pages/auth/otp-verfication/otp-verfication.component').then(
+        (m) => m.OtpVerificationComponent
+      ),
+  },
+  {
+    path: 'otp-complete',
+    loadComponent: () =>
+      import('./pages/auth/otp-complete/otp-complete.component').then(
+        (m) => m.OtpCompleteComponent
+      ),
+  },
+  {
+    path: 'password-reset',
+    loadComponent: () =>
+      import(
+        './pages/auth/password-reset-request/password-reset-request.component'
+      ).then((m) => m.PasswordResetRequestComponent),
+  },
+  {
+    path: 'password-reset-confirm',
+    loadComponent: () =>
+      import(
+        './pages/auth/password-reset-confirm/password-reset-confirm.component'
+      ).then((m) => m.PasswordResetConfirmComponent),
+  },
+  {
+    path: 'product-details',
+    loadComponent: () =>
+      import('./pages/product-details/product-details.component').then(
+        (m) => m.ProductDetailsComponent
+      ),
+  },
+  {
+    path: 'my-account',
+    loadComponent: () =>
+      import('./pages/my-account/my-account.component').then(
+        (m) => m.MyAccountComponent
+      ),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./pages/search-books-page/search-books-page.component').then(
+        (m) => m.SearchBooksPageComponent
+      ),
+  },
+  {
+    path: 'aboutus',
+    loadComponent: () =>
+      import('./components/about-us/about-us.component').then(
+        (m) => m.AboutUsComponent
+      ),
+  },
   {
     path: 'admin',
-    component: AdminComponent,
     canActivate: [AdminGuard],
+    loadComponent: () =>
+      import('./pages/admin/admin.component').then((m) => m.AdminComponent),
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardOverviewComponent },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import(
+            './pages/admin/components/dashboard-overview/dashboard-overview.component'
+          ).then((m) => m.DashboardOverviewComponent),
+      },
       // Books routes
-      { path: 'books', component: BookListComponent },
-      { path: 'books/new', component: BookFormComponent },
-      { path: 'books/edit/:id', component: BookFormComponent },
+      {
+        path: 'books',
+        loadComponent: () =>
+          import('./pages/admin/components/book-list/book-list.component').then(
+            (m) => m.BookListComponent
+          ),
+      },
+      {
+        path: 'books/new',
+        loadComponent: () =>
+          import('./pages/admin/components/book-form/book-form.component').then(
+            (m) => m.BookFormComponent
+          ),
+      },
+      {
+        path: 'books/edit/:id',
+        loadComponent: () =>
+          import('./pages/admin/components/book-form/book-form.component').then(
+            (m) => m.BookFormComponent
+          ),
+      },
       // Users routes
-      { path: 'users', component: UserListComponent },
-      { path: 'users/new', component: UserFormComponent },
-      { path: 'users/edit/:id', component: UserFormComponent },
-      { path: 'admins', component: AdminListComponent },
+      {
+        path: 'users',
+        loadComponent: () =>
+          import('./pages/admin/components/user-list/user-list.component').then(
+            (m) => m.UserListComponent
+          ),
+      },
+      {
+        path: 'users/new',
+        loadComponent: () =>
+          import('./pages/admin/components/user-form/user-form.component').then(
+            (m) => m.UserFormComponent
+          ),
+      },
+      {
+        path: 'users/edit/:id',
+        loadComponent: () =>
+          import('./pages/admin/components/user-form/user-form.component').then(
+            (m) => m.UserFormComponent
+          ),
+      },
+      {
+        path: 'admins',
+        loadComponent: () =>
+          import(
+            './pages/admin/components/admin-list/admin-list.component'
+          ).then((m) => m.AdminListComponent),
+      },
       {
         path: 'categories',
         loadComponent: () =>
@@ -150,6 +279,11 @@ export const routes: Routes = [
         (m) => m.ProductDetailsComponent
       ),
   },
-
-  { path: '**', component: NotFoundComponent },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
 ];
